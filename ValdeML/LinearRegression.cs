fe1d6534ebe176e0e77caac1edb0e2fd54321202
angularnodedeveloper <raph.games@hotmail.com>
2023-10-09 20:59:20 +0300

    VALDE

--- ValdeML/LinearRegression.cs
+++ ValdeML/LinearRegression.cs
@@ -130,29 +130,104 @@ namespace ValdeML
 
         public double[] InputDerivatives(Grad grad, double[] inputs)
         {
-            throw new NotImplementedException();
+            int size = inputs.Length;
+            double[] input_derivs = new double[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                double input_deriv = grad.derivs[i] * inputs[i];
+                input_derivs[i] = input_deriv;
+            }
+
+            return input_derivs;
         }
 
         public double[] ErrorDerivatives(Grad grad, double[] targets)
         {
-            throw new NotImplementedException();
+            int size = targets.Length;
+            double[] error_derivs = new double[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                double error_deriv = 2 * (grad.preds[i] - targets[i]);
+                error_derivs[i] = error_deriv;
+            }
+
+            return error_derivs;
         }
 
 
         public double[] Errors(Grad grad, double[] targets)
         {
-            throw new NotImplementedException();
+            int size = targets.Length;
+            double[] errors = new double[size];
+
+            for(int i = 0; i < size; i++)
+            {
+                double error = Math.Pow(grad.preds[i] - targets[i], 2);
+                errors[i] = error;
+            }
+
+            grad.error = errors.Sum() / (2 * targets.Length);
+            return errors;
         }
 
 
         public double[] Predictions(Grad grad, double[][] inputs)
         {
-            throw new NotImplementedException();
+            int o_size = inputs.Length;
+            int i_size = inputs[0].Length;
+            double[] predictions = new double[o_size];
+            for (int i = 0; i < o_size; i++)
+            {
+                double[] features = inputs[i];
+                double[] feature_calcs = new double[i_size];
+                for (int j = 0; j < i_size; j++)
+                {
+                    Wopt wopt = grad.ws[j];
+                    double feature_calc = wopt.w * features[i];
+                    feature_calcs[j] = feature_calc;
+                }
+                double prediction = feature_calcs.Sum() + grad.b.b;
+                predictions[i] = prediction;
+            }
+            return predictions;
         }
 
         public void Train(Grad grad, MMODEL[][] batches)
         {
-            throw new NotImplementedException();
+            Transposer tr = new Transposer();
+            grad.UpdateW(batches[0][0].input);
+            while (grad.error >= 0)
+            {
+                grad.epoch++;
+                for (grad.bid = 0; grad.bid < batches.Length; grad.bid++)
+                {
+                    MMODEL[] batch = batches[grad.bid];
+                    double[][] inputs = batch.Select(x => x.input).ToArray();
+                    double[] targets = batch.Select(x => x.target).ToArray();
+                    grad.d = batch.Length;
+
+                    grad.preds = Predictions(grad, inputs);
+                    grad.errors = Errors(grad, targets);
+                    grad.derivs = ErrorDerivatives(grad, targets);
+
+                    double[][] inputsT = tr.TransposeList(inputs);
+                    for (grad.fid = 0; grad.fid < inputsT.Length; grad.fid++)
+                    {
+                        Wopt wop = grad.ws[grad.fid];
+                        grad.input_derivs = InputDerivatives(grad, inputsT[grad.fid]);
+
+                        double tmp_w = wop.w - grad.a * grad.GetJW();
+                        wop.w = tmp_w;
+                    }
+                    Bopt bop = grad.b;
+                    double tmp_b = bop.b - grad.a * grad.GetJB();
+                    bop.b = tmp_b;
+                }
+                if (grad.error <= Math.Pow(10, -2))
+                    break;
+            }
         }
     }
 }
