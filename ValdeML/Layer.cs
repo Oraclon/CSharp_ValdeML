10bd1936e6e46c4a779ba89de191cd1f3e067c69
Valde <info@oraclon.gr>
2023-10-13 20:22:27 +0300

    Valde

---
+++ ValdeML/Layer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValdeML
+{
+    public class Layer
+    {
+        #region Layer Constructor
+        public Layer(int layer_size, int layer_id, Activation layer_activation, Optimizer Optimizer)
+        {
+
+        }
+        #endregion
+        #region Layer Variables
+        Node[] Nodes { get; set; }
+        public double[][] Predictions { get; set; }
+        public double[][] Activations { get; set; }
+        public double[][] ActDerivs { get; set; }
+        public double[][] NodeDerivs { get; set; }
+        public double[][] NodeDerivsPow { get; set; }
+        public double[][] NodeDerivsW { get; set; }
+        public string LayerId { get; set; }
+        #endregion
+    }
+}
--- ValdeML/Program.cs
+++ ValdeML/Program.cs
@@ -5,25 +5,7 @@ using Microsoft.VisualBasic;
 namespace ValdeML
 {   
     
-    public class Layer
-    {
-        #region Layer Constructor
-        public Layer(int layer_size, int layer_id, Activation layer_activation, Optimizer Optimizer) 
-        {
-            
-        }
-        #endregion
-        #region Layer Variables
-        Node[] Nodes                    { get; set; }
-        public double[][] Predictions   { get; set; }
-        public double[][] Activations   { get; set; }
-        public double[][] ActDerivs     { get; set; }
-        public double[][] NodeDerivs    { get; set; }
-        public double[][] NodeDerivsPow { get; set; }
-        public double[][] NodeDerivsW   { get; set; }
-        public string LayerId           { get; set; }
-        #endregion
-    }
+    
     
     class Program
     {
