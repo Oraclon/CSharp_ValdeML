8ea742fcb6cf60bcc14639f58eba3120348c4e90
Valde_Mac <info@oraclon.gr>
2023-10-15 23:36:46 +0300

    ValdeMac

---
+++ DevelopmentTests/FastLoop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace DevelopmentTests
+{
+	public static class FastLoop
+	{
+		public static void Run(int maxRange)
+		{
+            var random = new Random(400);
+            var items = Enumerable.Range(0, maxRange).Select(x => random.Next()).ToArray();
+            Span<int> listAsSpan = items;
+            ref var searchspace = ref MemoryMarshal.GetReference(listAsSpan);
+            for (var i = 0; i < listAsSpan.Length; i++)
+            {
+                var item = Unsafe.Add(ref searchspace, i);
+                Console.WriteLine($"{i} {item}");
+            }
+        }
+	}
+}
+
--- DevelopmentTests/Program.cs
+++ DevelopmentTests/Program.cs
@@ -1,10 +1,10 @@
-using System;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Microsoft.VisualBasic;
 
-namespace ValdeML
+namespace DevelopmentTests
 {
     static class DemoDataset
     {
@@ -15,7 +15,7 @@ namespace ValdeML
     {
         static void Main(string[] args)
         {
-            
+            FastLoop.Run(10000);
         }
     }
 }
