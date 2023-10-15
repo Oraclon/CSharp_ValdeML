bfc632bf4a8a530e090b68096f257a38b924ed7c
Valde_Mac <info@oraclon.gr>
2023-10-15 23:20:45 +0300

    ValdeMac

--- ValdeML/Layer.cs
+++ ValdeML/Layer.cs
@@ -97,25 +97,4 @@ namespace ValdeML
         }
         #endregion
     }
-    public static class Transposer2
-    {
-        public static double[][] TransposeList(double[][] inputs)
-        {
-            int outter_size   = inputs.Length;
-            int inner_size    = inputs[0].Length;
-
-            double[][] transposed = new double[inner_size][];
-
-            for (int i = 0; i < inner_size; i++)
-            {
-                double[] test = new double[outter_size];
-                for (int j = 0; j < outter_size; j++)
-                {
-                    test[j] = inputs[j][i];
-                }
-                transposed[i] = test;
-            }
-            return transposed;
-        }
-    }
 }
