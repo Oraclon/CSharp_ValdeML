f0271957c68993d54ca6aa57e03924068c469242
Raphael aka theScripter <73601567+Oraclon@users.noreply.github.com>
2025-05-16 14:42:59 +0300

    Update GaussianNoise.cs

--- GaussianNoise.cs
+++ GaussianNoise.cs
@@ -1,83 +1,109 @@
 using System;
-
-class GaussianNoiseGenerator
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using OpenCL.Net;
+public static class ImgProcessingTools
 {
-    private Random random = new Random();
-    private bool hasSpare = false;
-    private double spare;
-
-    public double NextGaussian(double mu = 0.0, double sigma = 1.0)
+    public static double Clamp(this double value, double min, double max)
     {
-        if (hasSpare)
+        return Math.Max(min, Math.Min(max, value));
+    }
+    public static double[] ApplyGaussianNoiseOnImage(this double[] pixels, double mu = 0, double sigma = 10)
+    {
+        GaussianNoiseGenerator gaussian = new GaussianNoiseGenerator();
+        double min = pixels.Min();
+        double max = pixels.Max();
+
+        for (int pixel = 0; pixel < pixels.Length; pixel++)
         {
-            hasSpare = false;
-            return spare * sigma + mu;
+            double noisy = pixels[pixel] + gaussian.NextGaussian(mu, sigma);
+            pixels[pixel] = noisy.Clamp(0, 1);
         }
 
-        double u, v, s;
+        return pixels;
+    }
+}
+public class GaussianNoiseGenerator
+{
+    private Random _Random = new Random();
+    public bool HasSpare = false;
+    private double _Spare = 0;
+    //--Sigma
+    //Small sigma(e.g., sigma = 0.01):
+
+    //The noise added to the image will be very small.
+
+    //The image will have barely noticeable noise.
+
+    //The pixel values will stay very close to the original values with only tiny random variations.
+
+    //Larger sigma (e.g., sigma = 0.1 or sigma = 0.2):
+
+    //The noise will become more pronounced.
+
+    //The image will show stronger noise with noticeable random variations in pixel values.
+
+    //--MU
+    //Case 1: mu = 0.0 (Zero Mean)
+    //If you add noise with mu = 0.0, the noise will be centered around zero, meaning:
+
+    //Some pixels will increase in value(brighten).
+
+    //Some pixels will decrease in value(darken).
+
+    //So, the image will have random variations around the original pixel values.
+
+    //Case 2: mu = 0.1 (Positive Mean)
+    //If you add noise with mu = 0.1, the noise will on average increase the pixel values by 0.1:
+
+    //The image will tend to brighten.
+
+    //Some pixels will still decrease slightly (due to the randomness of the noise), but overall, most pixels will become slightly brighter.
+
+    //Case 3: mu = -0.1 (Negative Mean)
+    //If you add noise with mu = -0.1, the noise will on average decrease the pixel values by 0.1:
+
+    //The image will tend to darken.
+
+    //Most pixels will become darker, but some will still increase slightly due to randomness.
+
+    public double NextGaussian(double mu = 0, double sigma = 1.0)
+    {
+        if (HasSpare)
+        {
+            HasSpare = false;
+            return _Spare * sigma + mu;
+        }
+
+        double u = 0;
+        double v = 0;
+        double s = 0;
 
         do
         {
-            u = 2.0 * random.NextDouble() - 1.0;
-            v = 2.0 * random.NextDouble() - 1.0;
+            u = 2 * _Random.NextDouble() - 1;
+            v = 2 * _Random.NextDouble() - 1;
             s = u * u + v * v;
         }
-        while (s >= 1.0 || s == 0.0);
+        while (s >= 1.0 || s == 0);
 
-        s = Math.Sqrt(-2.0 * Math.Log(s) / s);
-
-        spare = v * s;
-        hasSpare = true;
+        s = Math.Sqrt(-2 * Math.Log(s) / s);
+        _Spare = v * s;
+        HasSpare = true;
 
         return mu + sigma * u * s;
     }
 }
-
-class AddGaussianNoiseToImage
+class Programaa
 {
-    static int Clamp(double value, int min, int max)
-    {
-        return (int)Math.Max(min, Math.Min(max, Math.Round(value)));
-    }
-
     static void Main()
     {
-        int[,] image = new int[3, 3]
-        {
-            { 100, 120, 130 },
-            { 125, 135, 140 },
-            { 110, 115, 125 }
-        };
+        Random rand = new Random();
 
-        var noiseGen = new GaussianNoiseGenerator();
-        double mu = 0.0;
-        double sigma = 10.0;
-
-        Console.WriteLine("Original Image:");
-        PrintImage(image);
-
-        Console.WriteLine("\nNoisy Image:");
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                double noisy = image[i, j] + noiseGen.NextGaussian(mu, sigma);
-                image[i, j] = Clamp(noisy, 0, 255);
-            }
-        }
-
-        PrintImage(image);
-    }
-
-    static void PrintImage(int[,] image)
-    {
-        for (int i = 0; i < image.GetLength(0); i++)
-        {
-            for (int j = 0; j < image.GetLength(1); j++)
-            {
-                Console.Write($"{image[i, j],4}");
-            }
-            Console.WriteLine();
-        }
+        double[] pixels = new double[]{ 0.39, 0.47, 0.51, 0.49, 0.53, 0.55, 0.43, 0.45, 0.49 };
+        pixels.ApplyGaussianNoiseOnImage(0, .5);
     }
 }
